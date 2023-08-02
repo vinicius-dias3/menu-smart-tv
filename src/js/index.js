ac4e0ffb5b1941ab79e90eb2258acb6a5028777e
vinicius-dias3 <vinicius.sod3@gmail.com>
2023-08-02 09:49:58 -0300

    checkpoint

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,42 @@
-const apps = document.querySelectorAll('.app')
-let arrayApps = Array.from(apps)
-let maiorClick = 0
-let indiceMaiorClick = 0
-// let novoApps = []
-const paiDaArray = document.querySelector('.main')
+const apps = document.querySelectorAll('.app');
+let arrayApps = Array.from(apps);
+let maiorClick = 0;
+let indiceMaiorClick = 0;
+const paiDaArray = document.querySelector('.main');
+console.log(arrayApps)
 
-arrayApps.forEach((app, index)=> {
-    let contadorDeClick = parseInt(app.dataset.contador)
+arrayApps.forEach((app, index) => {
+    // app.dataset.index = index; //confirmar esta linha
+    let contadorDeClick = parseInt(app.dataset.contador); //ok. está pegando o contador do html
 
-    app.addEventListener('click', (event)=> {
-        event.preventDefault()
-        contadorDeClick++
-        
-        if(contadorDeClick > maiorClick){
-            maiorClick = contadorDeClick
-            indiceMaiorClick = index
-            console.log(`O maior clique agora é: ${maiorClick} do índice ${indiceMaiorClick}`)
+    app.addEventListener('click', (event) => {
+        event.preventDefault();
+        contadorDeClick++;
+        app.dataset.index = contadorDeClick
+        // console.log(arrayApps[4], parseInt(app.dataset.index))//pra saber se o contador de click funciona
+        //daqui pra cima, tudo funcionando
 
-            const elementoMaiorClick = arrayApps.splice(indiceMaiorClick, 1)[0]
-            arrayApps.unshift(elementoMaiorClick)
-            console.log(arrayApps)
-            
-            arrayApps.forEach((app)=> {
-                paiDaArray.appendChild(app)
-            })
+        if (contadorDeClick > maiorClick) {
+            maiorClick = contadorDeClick;
+            indiceMaiorClick = index;
+            // console.log(indiceMaiorClick) // testando - OK
+            // console.log(`O maior clique agora é: ${maiorClick} do índice ${indiceMaiorClick}`);
+
+            const elementoMaiorClick = arrayApps[indiceMaiorClick]
+            // console.log(elementoMaiorClick) // TESTANDO - OK
+            // if(indiceMaiorClick){
+                arrayApps.splice(indiceMaiorClick, 1);
+                arrayApps.unshift(elementoMaiorClick);
+                console.log(arrayApps);
+            // }
+                
+                // arrayApps.forEach((app) => {
+                //     paiDaArray.appendChild(app);
+                // });
         }
-    })
-})
+    });
+});
+
 
 
 //maneira diferente de escrever a mesma lógica.
